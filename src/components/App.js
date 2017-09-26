3ed2494d8afe43e3c2b0a07e8a11b0cb670a3fee
Chineze Nwosu <chineze.nwosu@andela.com>
2017-09-26 19:44:28 +0100

    add list item feature and toggle functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,166 +1,212 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { State } from 'slate';
 import { Editor } from 'slate-react';
-import { State, Block } from 'slate';
 import editList from 'slate-edit-list';
+import stateJson from './initialState.json';
 
-import initialState from './initialState.json';
+const plugin = editList();
+const plugins = [plugin];
 
-const listPlugin = editList({
-	listType: 'ul_list',
-	itemType: 'list_item',
-	defaultType: 'paragraph',
-});
-const plugins = [listPlugin];
+// To update the highlighting of nodes inside the selection
+// highlightedItems.suppressShouldComponentUpdate = true;
 
 let self;
 
 class TextEditor extends Component {
-
-	constructor(props) {
+    constructor(props) {
 		super(props);
-		self = this;
+        self = this;
+
 		this.state = {
-			content: State.fromJSON(initialState),
-			listItemsToggleStatus: [],
+			state: State.fromJSON(stateJson),
+            parentItems: {}
 		};
-		this.schema = {
-			nodes: {
-				'section': (props) => <div {...props.attrs}>{props.children}</div>,
-				'title': (props) => <h3 {...props.attrs}>{props.children}</h3>,
-				'paragraph': (props) => <p {...props.attrs}>{props.children}</p>,
-				'span': (props) => <span {...props.attrs}>{props.children}</span>,
-				'ul_list': (props) => <ul {...props.attrs}>{props.children}</ul>,
-				'ol_list': (props) => <ol {...props.attrs}>{props.children}</ol>,
-				'list_item': self.processListItems,
-			},
-			marks: {
-				bold: {
-					fontWeight: 'bold',
-				},
-				code: {
-					fontFamily: 'monospace',
-					backgroundColor: '#eee',
-					padding: '3px',
-					borderRadius: '4px',
-				},
-				italic: {
-					fontStyle: 'italic',
-				},
-				underlined: {
-					textDecoration: 'underline',
-				},
-			},
-			rules: [{
-				/* Rule that forces at least one paragraph, normalizes by inserting an empty paragraph */
-				match: (node) => node.kind === 'document',
-				validate: (document) => document.nodes.size < 2 ? true : null,
-				normalize: (change, document) => {
-					const paragraph = Block.create({ type: 'paragraph', data: {} });
-					return change.insertNodeByKey(document.key, 1, paragraph);
-				},
-			}],
-		};
-		this.processListItems.suppressShouldComponentUpdate = true;
-	};
-	hasBlock(type) {
-		const state = this.state.content;
-		return state.blocks.some((node) => node.type == type);
-	};
-
-	onChange({ state }) {
-		self.setState({ content: state });
-	};
-
-	processListItems(props) {
-		const { state, node } = props;
-		const depth = listPlugin.utils.getItemDepth(state);
-		const text = props.children[0].props.node.text;
-		// add a minimize prop to item if it does not exist
-		let itemStatusIndex = self.state.listItemsToggleStatus.findIndex((item) => item.key === node.key);
-		if (itemStatusIndex < 0) {
-			self.state.listItemsToggleStatus.push({
-				key: node.key,
-				minimize: false,
-			});
-			itemStatusIndex = self.state.listItemsToggleStatus.length - 1;
-		}
-		return (
-			<li
-				{...props.attributes}>
-				<img
-				onClick={() => self.toggleListItem(itemStatusIndex)}
-				className='list-icon'
-				src={self.state.listItemsToggleStatus[itemStatusIndex].minimize ? './img/arrow_right.svg' : './img/arrow_down.svg'}
-			/>
-				{ self.state.listItemsToggleStatus[itemStatusIndex].minimize ? <span>{text}</span> : props.children }
-			</li>
-		);
-	};
-
-	toggleListItem(index) {
-		let listItemsToggleStatus = this.state.listItemsToggleStatus;
-		listItemsToggleStatus[index].minimize = !listItemsToggleStatus[index].minimize;
-		this.setState({
-			listItemsToggleStatus,
-		});
 	}
 
-	onKeyDown(e, data, change) {
-		if (!data.isMod) return;
-		let mark;
+    onChange({ state }) {
+        self.setState({
+            state
+        });
+    }
 
-		switch (data.key) {
-			case 'b':
-			mark = 'bold';
-			break;
-			case 'i':
-			mark = 'italic';
-			break;
-			case 'u':
-			mark = 'underlined';
-			break;
-			case '`':
-			mark = 'code';
-			break;
-			default:
-			return;
-		};
+    call(change) {
+        self.setState({
+            state: this.state.state.change().call(change).state
+        });
+    }
 
-		e.preventDefault();
-		change.toggleMark(mark);
-		return true;
-	};
+    toggleListItem(event, key, state) {
+        let parentItems = self.state.parentItems;
+        parentItems[key] = !this.state.parentItems[key];
 
-	renderEditor() {
-		return (
-			<div className="editor">
-				<Editor
-					state={this.state.content}
-					onChange={this.onChange}
-					onKeyDown={this.onKeyDown}
-					schema={this.schema}
-					plugins={plugins}
-					placeholder={'Enter some text...'}
-					spellCheck
-				/>
-			</div>
-		);
-	}
+        // Hack to focus on editor or this would not work until one of the items is clicked.
+        this.editor.focus();
 
-	render() {
-		return (
-			<section className="modal">
-				<h3>TITLE</h3>
-				<textarea
-					className="textarea"
-					placeholder="50M with h/o DM c/o substernal chest pain. Do I need an EKG?"
-				/>
+        self.setState({
+            parentItems: parentItems
+        });
+    }
 
-				<h3>CASE</h3>
-				{this.renderEditor()}
-			</section>
-		);
-	}
-}
+    highlightedItems(props) {
+        const { node, state } = props;
+        const isCurrentItem = plugin.utils.getItemsAtRange(state).contains(node);
+        const depth = plugin.utils.getItemsAtRange(state)
+        const hasChildItems = props.children.length > 1
+
+        let classNames = isCurrentItem ? 'current-item' : '';
+
+        if (hasChildItems) {
+            classNames = this.state.parentItems[node.key] ? 'closed parent-item' : `${classNames} open parent-item`;
+        }
+
+        return (
+            <li className={classNames}
+                title={isCurrentItem ? 'Current Item' : ''}
+                {...props.attributes}>
+                {
+                    hasChildItems &&
+                    <img
+                        onClick={(event) => this.toggleListItem(event, node.key, state)}
+                        className='list-icon'
+                        src={this.state.parentItems[node.key] ? './img/arrow_right.svg' : './img/arrow_down.svg'}
+                    />
+                }
+                {props.children}
+            </li>
+        );
+    };
+
+    schema() {
+        return {
+            nodes: {
+                ul_list:   props => <ul {...props.attributes}>{props.children}</ul>,
+                ol_list:   props => <ol {...props.attributes}>{props.children}</ol>,
+                list_item: props => this.highlightedItems(props)
+            }
+        }
+    }
+
+    renderToolbar() {
+        const { wrapInList, unwrapList, increaseItemDepth, decreaseItemDepth } = plugin.changes;
+        const inList = plugin.utils.isSelectionInList(this.state.state);
+
+        return (
+            <div className='toolbar'>
+                <button className={inList ? 'active' : ''} onClick={() => this.call(inList ? unwrapList : wrapInList)}>
+                    <i className="fa fa-list-ul fa-lg"></i>
+                </button>
+
+                <button className={inList ? '' : 'disabled'} onClick={() => this.call(decreaseItemDepth)}>
+                    <i className="fa fa-outdent fa-lg"></i>
+                </button>
+
+                <button className={inList ? '' : 'disabled'} onClick={() => this.call(increaseItemDepth)}>
+                    <i className="fa fa-indent fa-lg"></i>
+                </button>
+
+                <button onClick={() => this.call(wrapInList)}>Wrap in list</button>
+                <button onClick={() => this.call(unwrapList)}>Unwrap from list</button>
+            </div>
+        );
+    }
+
+    render() {
+        return (
+            <div className="modal">
+                {this.renderToolbar()}
+                <h3>TITLE</h3>
+                <textarea
+                    value="13 year old with a Type 1 Diabetes presenting high glucose level and heart rate."
+                    onChange={() => {}}
+                    className="textarea"
+                    placeholder="50M with h/o DM c/o substernal chest pain. Do I need an EKG?"
+                />
+
+                <h3>CASE</h3>
+                <Editor
+                    placeholder={'Enter some text...'}
+                    ref={(editor) => { this.editor = editor; }}
+                    plugins={plugins}
+                    state={this.state.state}
+                    onChange={this.onChange}
+                    schema={this.schema()}
+                    onDrop={(evt, data, state) => {
+                        console.log({evt, data, state})
+                        const newState = state
+                        .transform()
+                        .deselect()
+                        .select(data.target)
+                        .insertText(' Drop me in the text ')
+                        .apply()
+
+                        return newState
+                    }}
+                />
+            </div>
+        );
+    }
+};
 
 export default TextEditor;
+
+
+/*import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Editor } from 'slate-react';
+import Plain from 'slate-plain-serializer';
+
+class MyEditor extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      state: Plain.deserialize('A bit of content in a Slate editor.')
+    }
+  }
+
+  onChange({ state }) {
+    this.setState({ state })
+  }
+
+  render() {
+    return (
+      <div>   
+        <div 
+          style={{ 
+          	background: 'black', 
+            color: 'white', 
+            width: 150, 
+            height: 20, 
+            textAlign: 'center',
+            marginBottom: 20
+           }} 
+          draggable
+          onDragStart={evt => {
+          	evt.dataTransfer.setData('text/plain', ' ')
+         	}}
+        >Drop me in the text</div>
+        <Editor
+          placeholder="Enter some text..."
+          
+          onChange={state => this.onChange(state)}
+          onDrop={(evt, data, state) => {
+          	console.log({evt, data, state})
+            const newState = state
+            .transform()
+            .deselect()
+            .select(data.target)
+            .insertText(' Drop me in the text ')
+            .apply()
+
+						return newState
+          }}
+          state={this.state.state}
+        />
+    	</div>
+    )
+  }
+
+}
+
+export default MyEditor;*/

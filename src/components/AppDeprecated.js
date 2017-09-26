765b7399446913bb850a2c66f921148b15762f65
Chineze Nwosu <chineze.nwosu@andela.com>
2017-09-26 10:05:31 +0100

    deprecate previous list implementation

---
+++ src/components/AppDeprecated.js
@@ -0,0 +1,166 @@
+import React, { Component } from 'react';
+import { Editor } from 'slate-react';
+import { State, Block } from 'slate';
+import editList from 'slate-edit-list';
+
+import initialState from './initialState.json';
+
+const listPlugin = editList({
+	listType: 'ul_list',
+	itemType: 'list_item',
+	defaultType: 'paragraph',
+});
+const plugins = [listPlugin];
+
+let self;
+
+class TextEditor extends Component {
+
+	constructor(props) {
+		super(props);
+		self = this;
+		this.state = {
+			content: State.fromJSON(initialState),
+			listItemsToggleStatus: [],
+		};
+		this.schema = {
+			nodes: {
+				'section': (props) => <div {...props.attrs}>{props.children}</div>,
+				'title': (props) => <h3 {...props.attrs}>{props.children}</h3>,
+				'paragraph': (props) => <p {...props.attrs}>{props.children}</p>,
+				'span': (props) => <span {...props.attrs}>{props.children}</span>,
+				'ul_list': (props) => <ul {...props.attrs}>{props.children}</ul>,
+				'ol_list': (props) => <ol {...props.attrs}>{props.children}</ol>,
+				'list_item': self.processListItems,
+			},
+			marks: {
+				bold: {
+					fontWeight: 'bold',
+				},
+				code: {
+					fontFamily: 'monospace',
+					backgroundColor: '#eee',
+					padding: '3px',
+					borderRadius: '4px',
+				},
+				italic: {
+					fontStyle: 'italic',
+				},
+				underlined: {
+					textDecoration: 'underline',
+				},
+			},
+			rules: [{
+				/* Rule that forces at least one paragraph, normalizes by inserting an empty paragraph */
+				match: (node) => node.kind === 'document',
+				validate: (document) => document.nodes.size < 2 ? true : null,
+				normalize: (change, document) => {
+					const paragraph = Block.create({ type: 'paragraph', data: {} });
+					return change.insertNodeByKey(document.key, 1, paragraph);
+				},
+			}],
+		};
+		this.processListItems.suppressShouldComponentUpdate = true;
+	};
+	hasBlock(type) {
+		const state = this.state.content;
+		return state.blocks.some((node) => node.type == type);
+	};
+
+	onChange({ state }) {
+		self.setState({ content: state });
+	};
+
+	processListItems(props) {
+		const { state, node } = props;
+		const depth = listPlugin.utils.getItemDepth(state);
+		const text = props.children[0].props.node.text;
+		// add a minimize prop to item if it does not exist
+		let itemStatusIndex = self.state.listItemsToggleStatus.findIndex((item) => item.key === node.key);
+		if (itemStatusIndex < 0) {
+			self.state.listItemsToggleStatus.push({
+				key: node.key,
+				minimize: false,
+			});
+			itemStatusIndex = self.state.listItemsToggleStatus.length - 1;
+		}
+		return (
+			<li
+				{...props.attributes}>
+				<img
+				onClick={() => self.toggleListItem(itemStatusIndex)}
+				className='list-icon'
+				src={self.state.listItemsToggleStatus[itemStatusIndex].minimize ? './img/arrow_right.svg' : './img/arrow_down.svg'}
+			/>
+				{ self.state.listItemsToggleStatus[itemStatusIndex].minimize ? <span>{text}</span> : props.children }
+			</li>
+		);
+	};
+
+	toggleListItem(index) {
+		let listItemsToggleStatus = this.state.listItemsToggleStatus;
+		listItemsToggleStatus[index].minimize = !listItemsToggleStatus[index].minimize;
+		this.setState({
+			listItemsToggleStatus,
+		});
+	}
+
+	onKeyDown(e, data, change) {
+		if (!data.isMod) return;
+		let mark;
+
+		switch (data.key) {
+			case 'b':
+			mark = 'bold';
+			break;
+			case 'i':
+			mark = 'italic';
+			break;
+			case 'u':
+			mark = 'underlined';
+			break;
+			case '`':
+			mark = 'code';
+			break;
+			default:
+			return;
+		};
+
+		e.preventDefault();
+		change.toggleMark(mark);
+		return true;
+	};
+
+	renderEditor() {
+		return (
+			<div className="editor">
+				<Editor
+					state={this.state.content}
+					onChange={this.onChange}
+					onKeyDown={this.onKeyDown}
+					schema={this.schema}
+					plugins={plugins}
+					placeholder={'Enter some text...'}
+					spellCheck
+				/>
+			</div>
+		);
+	}
+
+	render() {
+		return (
+			<section className="modal">
+				<h3>TITLE</h3>
+				<textarea
+					className="textarea"
+					placeholder="50M with h/o DM c/o substernal chest pain. Do I need an EKG?"
+				/>
+
+				<h3>CASE</h3>
+				{this.renderEditor()}
+			</section>
+		);
+	}
+}
+
+export default TextEditor;

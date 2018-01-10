193cbf1aa28ef7d7b19b7e0f99e27ec59802a0a9
Chineze Nwosu <chineze.nwosu@andela.com>
2018-01-10 15:34:41 +0100

    Upgrade to latest slate version

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { State, Block } from 'slate';
+import { Value, Block } from 'slate';
 import { Editor } from 'slate-react';
 import editList from 'slate-edit-list';
 import stateJson from './initialState.json';
@@ -9,40 +9,34 @@ import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import immutable from 'immutable';
 
 const options = {
-    types: ['categories'],
-    typeItem: 'name'
+    types: ['list'],
+    typeItem: 'entity'
 }
-const plugin = editList(options);
-const plugins = [plugin];
+const editListPlugin = editList(options);
+const plugins = [editListPlugin];
 
 // To update the highlighting of nodes inside the selection
 // highlightedItems.suppressShouldComponentUpdate = true;
 
-let self;
-
 class TextEditor extends Component {
     constructor(props) {
 		super(props);
-        self = this;
 
 		this.state = {
-			state: State.fromJSON(stateJson),
+			value: Value.fromJSON(stateJson),
             parentItems: {}
 		};
+
+        this.onChange = this.onChange.bind(this)
+        this.renderNode = this.renderNode.bind(this)
+        this.toggleListItem = this.toggleListItem.bind(this)
+        this.handleEnterInput = this.handleEnterInput.bind(this)
 	}
 
-    onChange({ state }) {
-        self.setState({
-            state
-        });
+    onChange({ value }) {
+        this.setState({ value });
     }
-
-    call(change) {
-        self.setState({
-            state: this.state.state.change().call(change).state
-        });
-    }
-
+    
     toggleListItem(event, key, state) {
         let parentItems = this.state.parentItems;
         parentItems[key] = !this.state.parentItems[key];
@@ -50,7 +44,7 @@ class TextEditor extends Component {
         // Hack to focus on editor or this would not work until one of the items is clicked.
         this.editor.focus();
 
-        self.setState({
+        this.setState({
             parentItems: parentItems
         });
     }
@@ -76,66 +70,54 @@ class TextEditor extends Component {
     }
 
     highlightedList(props) {
-        const SortableList = SortableContainer(() =>
-            <ul {...props.attributes}>{props.children}</ul>
-        )
-
-        return (
-            <SortableList
-                onSortMove={() => this.editor.blur()}
-                onSortEnd={this.onSortEnd.bind(this, props)}
-                lockAxis={'y'}
-                lockToContainerEdges
-                useDragHandle
-                distance={5}
-            />
-        )
+        const {node, editor} = props;
+        const value = editor.value
+        const parent = value.document.getParent(node.key);
+        console.log("node", parent.kind)
+        return <ul {...props.attributes}>{props.children}</ul>
     }
 
     highlightedItems(props) {
-        const { node, state } = props;
-        const isCurrentItem = plugin.utils.getItemsAtRange(state).contains(node)
-        const depth = plugin.utils.getItemDepth(state)
+        const {node, editor} = props;
+        const value = editor.value
+        const isCurrentItem = editListPlugin.utils.getItemsAtRange(value).contains(node)
+        const depth = editListPlugin.utils.getItemDepth(value)
         const hasChildItems = props.children.length > 1
 
         // Only make the first depth of list items draggable.
-        const parent = state.document.getParent(node.key)
+        const parent = value.document.getParent(node.key)
         const index = parent.nodes.indexOf(node)
-        const isFirstParent = state.document.nodes.get('0').key === parent.key
+        const isFirstParent = value.document.nodes.get('0').key === parent.key
         const isDraggable = parent.nodes.size > 1
         const isMinimized = this.state.parentItems[node.key]
         let classNames = isCurrentItem ? 'current-item' : ''
 
-        const ParentDragHandle = SortableHandle(() => {
-            return (
-                <span className="list-icon" contentEditable={false}>
-                    <img
-                        onClick={(event) => this.toggleListItem(event, node.key, state)}
-                        onMouseMove={() => this.editor.blur()}
-                        draggable={false}
-                        src={this.state.parentItems[node.key] ? './img/arrow_right.svg' : './img/arrow_down.svg'}
-                    />
-                    <div className="icon-hover">
-                        <span><strong>Drag</strong> to move</span>
-                        <span><strong>Click</strong> to {isMinimized ? 'open' : 'close'} menu</span>
-                    </div>
-                </span>
-            ) 
-        })
-
-        const DragHandle = SortableHandle(() => <span onMouseOver={() => this.editor.blur()}>:::</span>);
+        const ParentDragHandle = (
+            <span className="list-icon" contentEditable={false}>
+                <img
+                    onClick={(event) => this.toggleListItem(event, node.key, value)}
+                    onMouseMove={() => this.editor.blur()}
+                    draggable={false}
+                    src={this.state.parentItems[node.key] ? './img/arrow_right.svg' : './img/arrow_down.svg'}
+                />
+                <div className="icon-hover">
+                    <span><strong>Drag</strong> to move</span>
+                    <span><strong>Click</strong> to {isMinimized ? 'open' : 'close'} menu</span>
+                </div>
+            </span>
+        )
 
         if (hasChildItems) {
             classNames = isMinimized ? `${classNames} closed parent-item` : `${classNames} open parent-item`;
         }
         classNames = isFirstParent ? `${classNames} first-parent-item` : classNames
 
-        const SortableItem = SortableElement(() =>
+        return (
             <li className={classNames}
                 title={isCurrentItem ? 'Current Item' : ''}
                 {...props.attributes}>
-                {hasChildItems && <ParentDragHandle />}
-                {!hasChildItems && isDraggable && <div className='drag-icon'><DragHandle /></div>}
+                {hasChildItems && ParentDragHandle}
+                {!hasChildItems && isDraggable && <div className='drag-icon'><span onMouseOver={() => this.editor.blur()}>:::</span></div>}
                 {
                     isDraggable &&
                     hasChildItems &&
@@ -143,57 +125,29 @@ class TextEditor extends Component {
                         <span contentEditable={false}>:::</span>
                     </div>
                 }
+                {console.log("props.children", props.children)}
                 {props.children}
             </li>
-        )
-
-        return (
-            <SortableItem className={classNames} index={index} />
         );
     }
 
-    schema() {
-        return {
-            nodes: {
-                categories:   props => this.highlightedList(props),
-                name: props => this.highlightedItems(props)
-            }
+    renderNode(props) {
+        const { attributes, children, node } = props;
+        switch (node.type) {
+            case "list": return this.highlightedList(props);
+            case "entity": return this.highlightedItems(props);
         }
-    }
-
-    renderToolbar() {
-        const { wrapInList, unwrapList, increaseItemDepth, decreaseItemDepth } = plugin.changes;
-        const inList = plugin.utils.isSelectionInList(this.state.state);
-
-        return (
-            <div className='toolbar'>
-                <button className={inList ? 'active' : ''} onClick={() => this.call(inList ? unwrapList : wrapInList)}>
-                    <i className="fa fa-list-ul fa-lg"></i>
-                </button>
-
-                <button className={inList ? '' : 'disabled'} onClick={() => this.call(decreaseItemDepth)}>
-                    <i className="fa fa-outdent fa-lg"></i>
-                </button>
-
-                <button className={inList ? '' : 'disabled'} onClick={() => this.call(increaseItemDepth)}>
-                    <i className="fa fa-indent fa-lg"></i>
-                </button>
-
-                <button onClick={() => this.call(wrapInList)}>Wrap in list</button>
-                <button onClick={() => this.call(unwrapList)}>Unwrap from list</button>
-            </div>
-        );
     }
 
     handleEnterInput(event) {
         if (event.key === 'Enter') {
-            const currentState = self.state.state
+            const currentState = this.state.state
             const parentNode = currentState.document.nodes.get('0')
             const indexToInsert = parentNode.nodes.size
             let listNode = 
             {
                 "kind": "block",
-                "type": "name",
+                "type": "entity",
                 "nodes": [
                     {
                         "kind": "block",
@@ -217,13 +171,18 @@ class TextEditor extends Component {
                 .insertNodeByKey(parentNode.key, indexToInsert, listNode)
                 .apply()
 
-            self[event.target.id].value = ''
-            self[event.target.id].blur()
-            self.setState({ state: state })
+            this[event.target.id].value = ''
+            this[event.target.id].blur()
+            this.setState({ state })
         }
     }
 
     render() {
+        console.log("this.state", this.state)
+        console.log("this.state.state", !this.state.value)
+        if (!this.state.value) {
+            return <span>Loading...</span>
+        }
         return (
             <div className="modal">
                 <h3>CASE</h3>
@@ -231,9 +190,9 @@ class TextEditor extends Component {
                     placeholder={'Enter some text...'}
                     ref={(editor) => { this.editor = editor; }}
                     plugins={plugins}
-                    state={this.state.state}
+                    value={this.state.value}
                     onChange={this.onChange}
-                    schema={this.schema()}
+                    renderNode={this.renderNode}
                 />
             </div>
         );
@@ -400,4 +359,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default TextEditor;
